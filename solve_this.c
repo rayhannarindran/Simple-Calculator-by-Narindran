16c49ad3fd18d8f0505427a73ecd85d3df9794a4
Narindran <88973632+rayhannarindran@users.noreply.github.com>
2021-12-04 12:13:58 +0700

    Update solve_this.c

--- solve_this.c
+++ solve_this.c
@@ -7,3 +7,7 @@ int main(){
     char op[] = "(0+0)*0+0^(0-0)";
     int angka[] = {6,5,4,3,2,1};
 }
+
+//Array op adalah representasi perhitungan yang akan dilakukan, dimana '0' adalah posisi dari nilai yang akan di proses.
+//dan tanda ( ) */ + - adalah operator perhitungannya, dan memilki prioritas tertentu.
+//Arrat angka adalah nilai yang akan diproses sesuai dengan proses di array operator.
